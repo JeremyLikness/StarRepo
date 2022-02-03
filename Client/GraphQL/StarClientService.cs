64ee41aeb18d6eea33e95c336ec574a3162e3414
Jeremy Likness <jeremy@jeremylikness.com>
2022-02-03 15:09:09 -0800

    Initial client code

---
+++ Client/GraphQL/StarClientService.cs
@@ -0,0 +1,32 @@
+﻿using StarRepo.Client.Pages;
+using StarRepo.Domain;
+using StarRepo.GraphQL;
+
+namespace StarRepo.Client.GraphQL
+{
+    public class StarClientService
+    {
+        private readonly StarClient client = null!;
+
+        public StarClientService(StarClient client)
+            => this.client = client ?? throw new ArgumentNullException(nameof(client));
+
+        public async Task<Telescope[]> GetTelescopesAsync()
+        {
+            var result = await client.GetTelescopes.ExecuteAsync();
+            if (result == null || result.Data == null)
+            {
+                return Array.Empty<Telescope>();
+            }
+            return result.Data.Telescopes.Select(t =>
+            new Telescope
+            {
+                Id = t.Id,
+                Manufacturer = t.Manufacturer,
+                Model = t.Model,
+                ApertureMM = t.ApertureMM,
+                FocalLengthMM = t.FocalLengthMM
+            }).ToArray();
+        }
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using StarRepo.Client;
+using StarRepo.Client.GraphQL;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddStarClient() 
+    .ConfigureHttpClient(client => client.BaseAddress = new Uri($"{builder.HostEnvironment.BaseAddress}graphql"));
+builder.Services.AddScoped<StarClientService>();
 await builder.Build().RunAsync();
